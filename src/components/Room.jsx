aa80771e31e95469bcc9df218d8249798f949e07
nicolrem <nicolas.remise@darva.com>
2020-03-07 16:24:14 +0100

    feat(): add project

---
+++ src/components/Room.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Room = () => {
+  return (
+    <div className="room">La chambre est disponible</div>
+  );
+}
+
+export default Room
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Room from './components/Room';
+
+ReactDOM.render(<Room/>, document.querySelector('#root'));
